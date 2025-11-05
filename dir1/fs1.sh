87bc2ed6c5660b92ac28d11f948a06c10371d541
EC2 Default User <ec2-user@ip-172-31-20-13.ap-southeast-2.compute.internal>
2025-11-05 08:01:37 +0000

    Added content in branch

---
+++ dir1/fs1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "Enter your marks"
+read marks
+
+if [ $marks -gt 90 ]
+then
+    echo "Distinction"
+elif [ $marks -ge 75 ] && [ $marks -le 90 ]
+then
+    echo "First class"
+elif [ $marks -ge 60 ] && [ $marks -lt 75 ]
+then
+    echo "Second class"
+elif [ $marks -ge 35 ] && [ $marks -lt 60 ]
+then
+    echo "Pass"
+else
+    echo "Fail"
+fi
+
